dc6ad2c03a84b0029ae24adf76dd9f018e2ef185
bradleypollard <bradley.pollard12@alumni.imperial.co.uk>
2018-08-11 22:30:19 +0100

    Last 3 gate types. For now.

---
+++ Unity Project/Ludum Dare 42/Assets/Scripts/Game/Gates/AddGate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGate : Gate
+{
+	public AddGate( CellCoordinates _coordinates, ObjectOrientation _orientation )
+		: base( FindFootprint( _coordinates, _orientation ), FindInputs( _coordinates, _orientation ), FindOutputs( _coordinates, _orientation ), GateType.Add )
+	{
+	}
+
+	public override void DoOperation( int[] _inputs )
+	{
+		if ( _inputs.Length != 2 )
+		{
+			Debug.LogError( "AddGate: Expected 2 input got " + _inputs.Length );
+		}
+
+		int result = _inputs[0] + _inputs[1];
+		CurrentValues = new int[] { result };
+	}
+
+	private static CellCoordinates[] FindFootprint( CellCoordinates _coordinates, ObjectOrientation _orientation )
+	{
+		return GridObjectOrientationHelper.Find1x1Footprint( _coordinates, _orientation );
+	}
+
+	private static CellCoordinates[] FindInputs( CellCoordinates _coordinates, ObjectOrientation _orientation )
+	{
+		CellCoordinates input1 = GridObjectOrientationHelper.Find1x1OffsetLeft( _coordinates, _orientation );
+		CellCoordinates input2 = GridObjectOrientationHelper.Find1x1OffsetUp( _coordinates, _orientation );
+		return new CellCoordinates[] { input1, input2 };
+	}
+
+	private static CellCoordinates[] FindOutputs( CellCoordinates _coordinates, ObjectOrientation _orientation )
+	{
+		CellCoordinates output = GridObjectOrientationHelper.Find1x1OffsetRight( _coordinates, _orientation );
+		return new CellCoordinates[] { output };
+	}
+}
