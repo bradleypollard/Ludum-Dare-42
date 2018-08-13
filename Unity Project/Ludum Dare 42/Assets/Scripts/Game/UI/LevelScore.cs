59fef5c16ef3fc39907a5b5f5dbd12a7f2dba0b3
RoboChiz <RoboChiz@users.noreply.github.com>
2018-08-13 20:08:09 +0100

    Home Button

--- Unity Project/Ludum Dare 42/Assets/Scripts/Game/UI/LevelScore.cs
+++ Unity Project/Ludum Dare 42/Assets/Scripts/Game/UI/LevelScore.cs
@@ -6,28 +6,32 @@ using UnityEngine.UI;
 public class LevelScore : MonoBehaviour
 {
 	public string LevelName = "";
-	public GameplayManager Manager;
+	private GameplayManager Manager;
 	public Text ScoreText;
-	public Button LevelButton;
+    private Button LevelButton;
 
 	// Use this for initialization
 	void Start()
 	{
 		Manager = FindObjectOfType<GameplayManager>();
+        LevelButton = GetComponent<Button>();
 
-		LevelButton.onClick.AddListener( delegate { Manager.StartGame( LevelName ); } );
+        LevelButton.onClick.AddListener( delegate { Manager.StartGame( LevelName ); } );
 	}
 
 	private void Update()
 	{
-		int score = PlayerPrefs.GetInt( LevelName + "Score", -1 );
-		if ( score >= 0 )
-		{
-			ScoreText.text = score.ToString();
-		}
-		else
-		{
-			ScoreText.text = "";
-		}
+        if (ScoreText != null)
+        {
+            int score = PlayerPrefs.GetInt(LevelName + "Score", -1);
+            if (score >= 0)
+            {
+                ScoreText.text = score.ToString() + " Cells Left";
+            }
+            else
+            {
+                ScoreText.text = "";
+            }
+        }
 	}
 }
